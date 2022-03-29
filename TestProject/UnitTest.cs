c94bb360137174b8b3eee98d4531a2cb76f9620d
TobiasHaidvogl <t.haidvogl@htlkrems.at>
2022-03-29 15:18:45 +0200

    feat: added UnitTests

---
+++ TestProject/UnitTest.cs
@@ -0,0 +1,35 @@
+using Model.Entities.OwnPattern;
+using NUnit.Framework;
+
+namespace TestProject;
+
+public class Tests {
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void TruckAndAddonTest() {
+        var truck = new Truck("Tobias", 10, 3, 3, 3);
+        Assert.AreEqual(truck.Code, "Tobias");
+        Assert.AreEqual(truck.Price, 10);
+        Assert.AreEqual(truck.CargoList.Length, 3);
+        Assert.AreEqual(truck.Speed, 3);
+        Assert.AreEqual(truck.MaxContainer, 3);
+
+        truck.Addon = new Addon("Tobi's Kriegsmaschine", 12, 3);
+
+        Assert.AreEqual(truck.Addon.Code, "Tobi's Kriegsmaschine");
+        Assert.AreEqual(truck.Addon.Price, 12);
+        Assert.AreEqual(truck.Addon.CargoList.Length, 3);
+    }
+
+    [Test]
+    public void VehicleFactoryTest() {
+        var vehicle = VehicleFactory.CreateCzolgTesli();
+        Assert.AreEqual(vehicle.Code, "Czolg Tesli");
+        Assert.AreEqual(vehicle.Price, 10);
+        Assert.AreEqual(vehicle.CargoList.Length, 4);
+        Assert.AreEqual(vehicle.Speed, 3);
+        Assert.AreEqual(vehicle.MaxContainer, 3);
+    }
+}
