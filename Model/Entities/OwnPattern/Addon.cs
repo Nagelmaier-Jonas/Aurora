49cf94e26d107060d761b0a80009d24f51ea1368
thom <t.lueger@htlkrems.at>
2022-04-01 10:37:49 +0200

    addons, VehicleFactory.cs updated

--- Model/Entities/OwnPattern/Addon.cs
+++ Model/Entities/OwnPattern/Addon.cs
@@ -1,5 +1,6 @@
 namespace Model.Entities.OwnPattern; 
 
 public class Addon : Vehicle{
+    public Addon(string code, int price, int slotCount, string imageUrl) : base(code, price, slotCount, imageUrl) { }
     public Addon(string code, int price, int slotCount) : base(code, price, slotCount) { }
 }
